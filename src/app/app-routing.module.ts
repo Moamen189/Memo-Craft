6ee8c1539b6142b437fccfbe627bc7e294999892
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-12-29 21:01:36 +0200

    Update Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlankLayoutComponent } from './Layouts/blank-layout/blank-layout.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AuthLayoutComponent } from './Layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { NotfpundComponent } from './Components/notfpund/notfpund.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{path:'' , component:BlankLayoutComponent , children:[
+  {path:'' , redirectTo:'home', pathMatch:'full'},
+
+  {path:'home', component:HomeComponent , title:'Home'}
+]},
+{path:'' , component:AuthLayoutComponent , children:[
+  {path:'' , redirectTo:'login', pathMatch:'full'},
+  {path:'login' , component:LoginComponent , title:'Login'},
+  {path:'register' , component:RegisterComponent , title:'Register'}
+]},
+{path:'**' , component:NotfpundComponent , title:'Not Found'}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
