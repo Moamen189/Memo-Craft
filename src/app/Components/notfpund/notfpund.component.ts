ccc378fb58e5781983df223b56ad59b40a829f39
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-12-29 14:48:03 +0200

    Update Components

---
+++ src/app/Components/notfpund/notfpund.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notfpund',
+  templateUrl: './notfpund.component.html',
+  styleUrls: ['./notfpund.component.scss']
+})
+export class NotfpundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { NoteDataComponent } from './Components/note-data/note-data.component';
+import { NotfpundComponent } from './Components/notfpund/notfpund.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    NoteDataComponent,
+    NotfpundComponent
   ],
   imports: [
     BrowserModule,
