b28a671efdeb1ed8cb30434ffc13cbf87edc92dd
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2024-02-10 19:09:13 +0200

    Update Dialog

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { NoteDataComponent } from '../note-data/note-data.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
+  openDialog() {
+    const dialogRef = this.dialog.open(NoteDataComponent);
 
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
--- src/app/Core/shared/shared.module.ts
+++ src/app/Core/shared/shared.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
   ],
-  exports:[MatInputModule , MatIconModule ,MatButtonModule]
+  exports:[MatInputModule , MatIconModule ,MatButtonModule ,MatDialogModule]
 })
 export class SharedModule { }
