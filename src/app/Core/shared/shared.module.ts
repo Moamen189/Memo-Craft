0c08716ee8d520b592460040dba3bb0a4cc530f0
Moamen ASHRAF <moamen.ashraf@intalio.com>
2024-02-11 14:42:28 +0200

    MatMenuModule

--- src/app/Core/shared/shared.module.ts
+++ src/app/Core/shared/shared.module.ts
@@ -4,12 +4,12 @@ import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDialogModule} from '@angular/material/dialog';
-
+import {MatMenuModule} from '@angular/material/menu';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
   ],
-  exports:[MatInputModule , MatIconModule ,MatButtonModule ,MatDialogModule]
+  exports:[MatInputModule , MatIconModule ,MatButtonModule ,MatDialogModule , MatMenuModule]
 })
 export class SharedModule { }
